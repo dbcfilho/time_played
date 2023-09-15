529dfa15e5a30bd17d191b82cbea1a23d688cd89
Douglas Costa <114622378+dbcfilho@users.noreply.github.com>
2023-09-15 14:17:17 -0300

    Add files via upload

---
+++ time_played.py
@@ -0,0 +1,8 @@
+tempo_de_jogo = int(input("Quanto tempo temos já jogado? "))
+
+if tempo_de_jogo <= 90:
+    print("Ainda tem jogo pela frente")
+    print("Que bom, eu amor counter strike")
+else:
+    print("Putz, corre que está acabando")
+    print("Vai acabar a luz!")
